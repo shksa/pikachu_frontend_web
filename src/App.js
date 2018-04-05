5e0fd62da9cf534f2eab10bea5327ce1c4c59a53
sreekar <sreekar339@gmail.com>
2018-04-06 00:53:11 +0530

    made transitions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import defaultImage from './default-image.png'
+import questionMarkImage from './DramaticQuestionMark.png'
 import { uploadImage } from './controller'
 import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { selectedFile: null, showErrorMsg: false }
+    this.state = { selectedFile: null, showErrorMsg: false, answer: null }
   }
 
   fileChangeHandler = (event) => {
@@ -15,7 +16,8 @@ class App extends Component {
 
   uploadHandler = () => {
     if (this.state.selectedFile) {
-      uploadImage(this.state.selectedFile)
+      const answerPromise = uploadImage(this.state.selectedFile)
+      answerPromise.then(answer => this.setState({ answer }))
     } else {
       this.setState({ showErrorMsg: true })
     }
@@ -25,17 +27,35 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header" >
-          Pikachu app
+          Pikachu
         </div>
         <div className="App-body" >
           <div className="App-body-img-selector" >
-            <img
-              src={this.state.selectedFile ? window.URL.createObjectURL(this.state.selectedFile) : defaultImage}
-              alt="selected img"
-              className="image-holder"
-            />
+            <div className="image-and-result">
+              <img
+                src={this.state.selectedFile ? window.URL.createObjectURL(this.state.selectedFile) : defaultImage}
+                alt="selected img"
+                className="image-holder"
+              />
+              {
+                this.state.selectedFile ?
+                (
+                  <div className="is-and-result" >
+                    <div className="is-holder" >
+                    is
+                    </div>
+                    <div className="result-holder">
+                      {this.state.answer ? this.state.answer : <img className="question-image" src={questionMarkImage} alt="questionmark" /> }
+                    </div>
+                  </div>
+                )
+                :
+                ''
+              }
+
+            </div>
             <input className="image-input" type="file" onChange={this.fileChangeHandler} />
-            <button className="upload-button" onClick={this.uploadHandler}>Upload!</button>
+            <button className="upload-button" onClick={this.uploadHandler}>Recognize!</button>
             <div className="error-msg">{this.state.showErrorMsg ? 'Please select an image and then upload' : ''}</div>
           </div>
         </div>
--- src/controller/uploadImage.js
+++ src/controller/uploadImage.js
@@ -1,13 +1,17 @@
 const uploadImage = (imageFile) => {
+  console.log('in uploadImage func')
   const formData = new FormData();
   formData.append('userName', 'guest');
   formData.append('userImage', imageFile);
-  fetch('/uploadImage', {
+  return fetch('/uploadImage', {
     method: 'POST',
     body: formData,
   })
     .then(resp => resp.text())
-    .then(text => console.log(text))
+    .then((text) => {
+      console.log(text)
+      return text
+    })
     .catch(error => console.log(error))
 };
 
