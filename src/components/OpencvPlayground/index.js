f4ee36ae2439734b67e640bf3b772785b259be8c
sreekar <sreekar339@gmail.com>
2018-04-07 22:31:18 +0530

    fixed css, added jsx for playground

--- src/components/OpencvPlayground/index.js
+++ src/components/OpencvPlayground/index.js
@@ -1,26 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './OpencvPlayground.css'
 
 class OpencvPlayground extends Component {
-
   constructor(props) {
     super(props)
-    this.state = {selectedFile: null}
+    this.canvasInput = null
+    this.canvasOutput = null
+  }
+
+  loadImageToCanvasInput = (imgUrl) => {
+    const canvasInputCtx = this.canvasInput.getContext('2d')
+    const img = new Image()
+    img.crossOrigin = 'anonymous'
+    img.onload = () => {
+      canvasInputCtx.drawImage(img, 0, 0, this.canvasInput.width, this.canvasInput.height)
+    }
+    img.src = imgUrl
+  }
+
+  selectImage = (event) => {
+    const imgFile = event.target.files[0]
+    const imgUrl = window.URL.createObjectURL(imgFile)
+    this.loadImageToCanvasInput(imgUrl)
+  }
+
+  setCanvasInputRef = (canvasInputElem) => {
+    this.canvasInput = canvasInputElem
+  }
+
+  setCanvasOutputRef = (canvasOutputElem) => {
+    this.canvasOutput = canvasOutputElem
   }
 
   render() {
     return (
-      <div className="opencv-playground" >
-        <div className="canvases-container" >
-          <canvas className="canvas-img-holder"></canvas>
-          <canvas className="canvas-img-holder"></canvas>
-        </div>
-        <div className="image-input" >
-          <button className="image-selection-button"> Pick an image </button>
-          <input type="file" onChange={this.setImageFile} />
-        </div>
-      </div>
-    ) 
+      // <div className="opencv-playground" >
+      //   <div className="canvases-container" >
+      //     <canvas ref={this.setCanvasInputRef} className="canvas-img-holder" />
+      //     <canvas ref={this.setCanvasOutputRef} className="canvas-img-holder" />
+      //   </div>
+      //   <div className="image-input" >
+      //     <button className="image-selection-button"> Pick an image </button>
+      //     <input type="file" onChange={this.selectImage} />
+      //   </div>
+      //   <button className="turn-grey-button" onClick={this.turnImageGrey}>Turn Image Grey</button>
+      // </div>
+      <p>lol</p>
+    )
   }
 }
 
